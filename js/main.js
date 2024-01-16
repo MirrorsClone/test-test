93ecaa484935c21a2e450ebce685cd8546ee2eae
Igor <email@email.com>
2024-01-16 18:40:56 +0200

    Lesson 37

--- js/main.js
+++ js/main.js
@@ -1,69 +1,78 @@
-// Задача 1
-let numberOfFilms;
-
-function start() {
-   numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
-
-   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-      numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
-   }
-}
-
-start();
-// Задача 2
 let personalMovieDB = {
-   count: numberOfFilms,
+
+   count: 0,
    movies: {},
    actors: {},
    genres: [],
-   pivat: false
-};
+   privat: true,
 
-function detectPersonalLevel() {
-   if (parseInt(personalMovieDB.count) < 10) {
-      alert('Просмотрено довольно мало фильмов')
-   } else if (parseInt(personalMovieDB.count) >= 10 && personalMovieDB.count <= 30) {
-      alert('Вы классический зритель')
-   } else if (parseInt(personalMovieDB.count) >= 30) {
-      alert('Вы киноман')
-   } else {
-      alert('error 2000')
-   }
-}
-detectPersonalLevel();
-
-// Задание 3
-
-function rememberMyFilms() {
-   let i = 1;
-
-   while (i <= 2) {
-      const movieOne = prompt('Один из послендних просмотренных фильмов?'),
-         filmRating = prompt('На сколько оцените его?');
-      if (movieOne === null || filmRating === null || filmRating === '' || movieOne === '' || movieOne.length > 50 || filmRating > 50) {
-         i--;
+   toggleVisibleMyDB: function () {
+      if (personalMovieDB.privat === false) {
+         personalMovieDB.privat = true;
       } else {
-         personalMovieDB.movies[movieOne] = filmRating;
+         personalMovieDB.privat = false
+      }
+   },
+
+   start: function () {
+      personalMovieDB.count = prompt('Сколько фильмов Вы уже посмотрели?');
+      while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+         personalMovieDB.count = prompt('Сколько фильмов Вы уже посмотрели?');
+      }
+   },
+
+   detectPersonalLevel: function () {
+      if (parseInt(personalMovieDB.count) < 10) {
+         alert('Просмотрено довольно мало фильмов');
+      } else if (parseInt(personalMovieDB.count) >= 10 && personalMovieDB.count <= 30) {
+         alert('Вы классический зритель');
+      } else if (parseInt(personalMovieDB.count) >= 30) {
+         alert('Вы киноман');
+      } else {
+         alert('error 2000');
+      }
+   },
+
+   rememberMyFilms: function () {
+      let i = 1;
+
+      while (i <= 2) {
+         const movieOne = prompt('Один из послендних просмотренных фильмов?'),
+            filmRating = prompt('На сколько оцените его?');
+         if (movieOne === null || filmRating === null || filmRating === '' || movieOne === '' || movieOne.length > 50) {
+            i--;
+         } else {
+            personalMovieDB.movies[movieOne] = filmRating;
+            i++;
+         };
       };
-      i++;
-   };
+   },
+
+   writeYourGenres: function () {
+      for (let i = 1; i <= 3; i++) {
+         const a = prompt(`Ваш любимый жанр под номером ${i}`)
+         if (a === null || a === '') {
+            i--;
+         } else {
+            personalMovieDB.genres[i - 1] = a;
+         }
+      }
+   },
+
+   showMyDB: function () {
+      if (personalMovieDB.privat === false) {
+         console.log(personalMovieDB);
+      }
+      personalMovieDB.genres.forEach((item, i) => {
+         console.log(`Любимый жанр #${i + 1} - это ${item}`)
+      })
+   },
+
 };
-rememberMyFilms();
-
-
-function writeYourGenres() {
-   for (let i = 1; i <= 3; i++) {
-      const a = prompt(`Ваш любимый жанр под номером ${i}`)
-      personalMovieDB.genres[i - 1] = a;
-   }
-}
-writeYourGenres();
-
-function showMyDB() {
-   if (personalMovieDB.pivat === false) {
-      console.log(personalMovieDB);
-   }
-}
-showMyDB();
-
-
+personalMovieDB.start();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
+alert(`Вы посмотрели: ${personalMovieDB.count} фильма.\nПросмотренные фильмы: ${JSON.stringify(personalMovieDB.movies, null, 2)}\nЖанры: ${JSON.stringify(personalMovieDB.genres, null, 2)}`)
